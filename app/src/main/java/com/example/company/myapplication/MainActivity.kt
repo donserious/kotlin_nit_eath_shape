120aa0ac8e94dc2ce3f62217aff49e17bf9ef96c
Aleksander Aksamentov <serious106@mail.ru>
2020-04-09 01:19:07 +0300

    #10022020@all_work

--- app/src/main/java/com/example/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.company.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         // Write your code here
 
+        butAnswer.setOnClickListener{btnClick()}
+
+    }
+
+
+
+    private fun btnClick() {
+        var idObj:Int = rgAnswers.checkedRadioButtonId
+        if (idObj!=-1) {
+            var rdButton: RadioButton = findViewById(idObj)
+            if (rdButton.text.toString() == "шар") {
+                tvAnswer.text = "Правильно!"
+            } else {
+                tvAnswer.text = "Неправильно!"
+            }
+        } else {
+            tvAnswer.text = "Неправильно!"
+        }
     }
 }
